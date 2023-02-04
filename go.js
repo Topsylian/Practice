0e8599cbc5a242f6e31395c222b75d377aaae158
Topsylian <topsylian@gmail.com>
2023-02-04 09:52:13 +0200

    hgggg

---
+++ go.js
@@ -0,0 +1,29 @@
+const containerText = document.querySelector("div#container");
+
+/*Selecting the input from the html doc*/
+const inputAge = document.querySelector(".input");
+const nextAge = document.querySelector(".container > p > span");
+
+inputAge.addEventListener("keyup", () => {
+  let val = inputAge.value;
+  
+  nextAge.textContent = 2023 - Number.parseInt(val, 10) + ' ' + 'years.';
+});
+
+let num = Math.floor(2.6);
+let count = "x";
+let answer = num + count; //Number.parseInt(count, 10);
+console.log(num);
+
+/*
+document.body.onmousemove = function (e) {
+  document.documentElement.style.setProperty(
+    "--x",
+    e.clientX + window.scrollX + "px"
+  );
+  document.documentElement.style.setProperty(
+    "--y",
+    e.clientY + window.scrollY + "px"
+  );
+};
+*/
